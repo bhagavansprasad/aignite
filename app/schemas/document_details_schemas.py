ba9c7f4a746ef3f25b96befc92e08f0b9788da61
bhagavan <bhagavan@auranetworks.in>
2025-03-07 13:54:59 +0530

    Added endpoint to get subjects and its chapters

--- app/schemas/document_details_schemas.py
+++ app/schemas/document_details_schemas.py
@@ -1,7 +1,7 @@
 # app/schemas/document_details_schemas.py
 
 from pydantic import BaseModel
-from typing import Optional, Dict
+from typing import Optional, Dict, List
 from datetime import datetime
 
 class DocumentDetailsCreate(BaseModel):
@@ -18,6 +18,19 @@ class DocumentDetailsResponse(BaseModel):
     full_metadata: Optional[Dict] = None
     created_at: datetime
     updated_at: datetime
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+
+class SubjectDetails(BaseModel):
+    id: int
+    gcs_file_id: int
+    subject: Optional[str]
+    chapters: Optional[List[Dict]]
+    uri_id: int
+    name: str
 
     class Config:
         orm_mode = True
